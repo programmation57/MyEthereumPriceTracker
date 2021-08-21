766d3d18a94d818c44a783fe5f885a661a226ec9
programmation57 <81985923+programmation57@users.noreply.github.com>
2021-08-21 23:58:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import smtplib, ssl
 
 url = 'https://coinmarketcap.com/fr/currencies/ethereum/'
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
+headers = {'User-Agent' : 'Votre User Agent}
 
 page = requests.get(url, headers=headers)
 
